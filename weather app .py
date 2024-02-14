7b10d258a18dcef1bfe461ac6d92e9efdc54a725
Golu667 <120365877+Golu667@users.noreply.github.com>
2024-02-14 21:36:56 +0530

    Add files via upload

---
+++ weather app .py
@@ -0,0 +1,73 @@
+from tkinter import *
+from tkinter import ttk
+import  requests#
+
+def data_get():
+    city = city_name.get()
+    data = requests.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=a9954f32a6253e4280d42f13c19e6c1c").json()
+    w_label1.config(text=data["weather"][0]["main"])
+    wd_label1.config(text=data["weather"][0]["description"])
+    temp_label1.config(text=str(int(data["main"]["temp"]-273.15)))
+    per_label1.config(text=data["main"]["pressure"])
+
+
+
+
+
+
+win = Tk()
+win.title("Weather Tech")
+win.config(bg = "blue")
+win.geometry("500x520")
+
+name_label=Label(win,text="Weather App",
+                 font=("time New Roman",30,"bold"))
+name_label.place(x=25,y=50,height=50,width=450)
+
+city_name = StringVar()
+list_name=["Andhra Pradesh","Arunachal Pradesh ","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal","Andaman and Nicobar Islands","Chandigarh","Dadra and Nagar Haveli","Daman and Diu","Lakshadweep","National Capital Territory of Delhi","Puducherry"]
+com = ttk.Combobox(win,text="Weather App", values=list_name,
+                 font=("time New Roman",20,"bold"),textvariable=city_name)
+
+com.place(x=25,y=120,height=50,width=450)
+
+
+w_label=Label(win,text="Weather Climate",
+                 font=("time New Roman",20))
+w_label.place(x=25,y=260,height=50,width=210)
+
+w_label1=Label(win,text="",
+                 font=("time New Roman",20))
+w_label1.place(x=250,y=260,height=50,width=210)
+
+wd_label=Label(win,text="Weather Description",
+                 font=("time New Roman",17))
+wd_label.place(x=25,y=330,height=50,width=210)
+
+wd_label1=Label(win,text="",
+                 font=("time New Roman",17))
+wd_label1.place(x=250,y=330,height=50,width=210)
+
+temp_label=Label(win,text="Tempreture",
+                 font=("time New Roman",20))
+temp_label.place(x=25,y=400,height=50,width=210)
+
+temp_label1=Label(win,text="",
+                 font=("time New Roman",20))
+temp_label1.place(x=250,y=400,height=50,width=210)
+
+per_label=Label(win,text="Pressure",
+                 font=("time New Roman",20))
+per_label.place(x=25,y=470,height=50,width=210)
+
+per_label1=Label(win,text="",
+                 font=("time New Roman",20))
+per_label1.place(x=250,y=470,height=50,width=210)
+
+
+done_button = Button(win,text="Done",
+                     font=("Time New Roman",20,"bold"),command=data_get)
+done_button.place(y=190,height=50,width=100,x=200)
+
+
+win.mainloop()
